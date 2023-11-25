21abc27d3a4ade11f2bbf3c96d70bc509fdb497a
amiralzd <123456amir78900@gmail.com>
2023-11-25 17:34:39 +0330

    nav

--- assets/js/jQuery.js
+++ assets/js/jQuery.js
@@ -1,4 +1,29 @@
 $(document).ready(function(){
+    $(".search").click(function(e){
+        e.stopPropagation();
+        $(".search-container").fadeIn();
+        $(".box").hide();
+        $(".bg-search-hover").fadeIn();
+        $("body").css({
+            "overflow": "hidden",
+        });
+    });
+
+    $(document).click(function() {
+        $(".search-container").fadeOut();
+        $(".box").show();
+        $(".bg-search-hover").fadeOut();
+        $("body").css({
+            "overflow": "auto",
+        });
+    });
+});
+
+
+
+
+
+$(document).ready(function(){
     $(".grouping-items").hover(
         function(){
             $('.grouping-items-hover-shadow').fadeIn();
